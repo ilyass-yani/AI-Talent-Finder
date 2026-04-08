1835839402a8593a020bbb0f4ae65a1461c89288
Omar Youbi <youbiomar20@gmail.com>
2026-04-08 19:24:34 +0100

    Delete frontend directory

--- frontend/src/components/Providers.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-import { ThemeProvider } from "@/hooks/useTheme";
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
-}
