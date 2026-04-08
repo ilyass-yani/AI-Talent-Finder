b5fa138db1ae5da7a7e5006e71bc1b9b44d4fc5c
Omar youbi <youbiomar20@gmail.com>
2026-04-08 18:43:53 +0100

    welp

--- frontend/src/components/Providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { AuthProvider } from "@/hooks/useAuth";
-import { ThemeProvider } from "@/hooks/useTheme";
-import RouteGuard from "@/components/RouteGuard";
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider><AuthProvider><RouteGuard>{children}</RouteGuard></AuthProvider></ThemeProvider>;
-}
